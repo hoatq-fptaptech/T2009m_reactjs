bb755f9f084a9c2548e9af8c5f473233b20cfd83
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-11-19 09:07:28 +0700

    up code react google map

--- src/Detail.js
+++ src/Detail.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import GoogleMap from 'google-map-react';
 export default function Detail() {
+    const center = {
+        lat: 21.02898,
+        lng: 105.7811
+    }
+    const zoom = 18;
     return (
         <section className="section main">
             <div className="container">
                 <article className="content">
                     <div className="body-content">
-                        <h1>Đây là 1 bài báo hay</h1>
-                        <img src="imgs/iphone_x_64gb_3.jpg" height={600} width={600} />
+                        <div style={{width:"1000px",height:"500px"}}>
+                            <GoogleMap center={center} zoom={zoom}
+                                       bootstrapURLKeys="AIzaSyCaj0mHlR-keu-9hsR6d-gB0L9BclG04rk"/>
+                        </div>
                     </div>
                 </article>
                 <aside className="sidebar">
